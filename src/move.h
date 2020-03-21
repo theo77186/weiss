ff5a3c1935349a809e3235023d44bf83ab6d2d85
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-03-21 14:22:08 +0100

    checkers for castling legality

--- src/move.h
+++ src/move.h
@@ -81,7 +81,7 @@ INLINE bool CastlePseudoLegal(const Position *pos, Color color, int side) {
 
     return (pos->castlingRights & castle)
         && !(pieceBB(ALL) & blocking)
-        && !SqAttacked(pos, kingSq, !color)
+        && !pos->checkers
         && !SqAttacked(pos, midway, !color);
 }
 
